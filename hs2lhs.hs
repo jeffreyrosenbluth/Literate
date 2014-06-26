d9937a41a968505708a158ce9328d37d32877033
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-06-25 23:43:04 -0400

    pragmas

--- hs2lhs.hs
+++ hs2lhs.hs
@@ -1,26 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf        #-}
 
 module Main where
 
 import            Control.Applicative ((<$>), (<|>))
 import            Data.Maybe          (fromMaybe)
-import            Data.Text           (Text, stripStart, stripPrefix)
+import            Data.Text           (Text, stripStart, stripPrefix,
+                                       isPrefixOf, isSuffixOf)
 import qualified  Data.Text           as T
 import qualified  Data.Text.IO        as T
 import            System.Environment
 
 data Current = Comment | Code
 
+stripPre :: Text -> Current -> Text -> Maybe (Current, Text)
 stripPre pre w t = g <$> stripPrefix pre t
   where g x = (w, stripStart x)
 
 lhsLine :: Current -> Text -> (Current, Text)
 lhsLine w t = fromMaybe d c
   where
-    c = stripPre "---" Comment t <|> stripPre "-- |" Comment t <|> stripPre "--" Comment t
-    d = if t == T.empty then (Code, "") else (Code, s)
-    s = p `T.append` t
-    p = case w of {Comment -> "\n> "; Code -> "> "}
+    d = if | t == T.empty -> (Code, "") 
+           | isPrefixOf "{-#" t && isSuffixOf "#-}" t -> (Code, t)
+           | otherwise -> (Code, s `T.append` t)
+    s = case w of {Comment -> "\n> "; Code -> "> "}
+    c = stripPre "---" Comment t <|> stripPre "-- |" Comment t 
+                                 <|> stripPre "--"   Comment t
 
 lhs :: Current -> [Text] -> [Text]
 lhs _ [] = []
